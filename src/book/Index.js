dabb5c7c30de6a3a64a10ec288e578a3ed05b794
ghosoonfk <ghosooonf@gmail.com>
2021-02-20 09:29:50 +0300

    display api book  with atheer moudi and ghosoon

--- src/book/Index.js
+++ src/book/Index.js
@@ -1,10 +1,40 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+           books: [],
+        }
+    }
+    
+    componentDidMount(){
+        axios.get("/bookclub/book/index")
+            .then(response =>{
+                console.log(response)
+                this.setState({
+                    books: response.data
+                })
+            })
+            .catch(error =>{
+                console.log(" Error book ");
+                console.log(error);
+            })
+
+
+    }
     render() {
         return (
             <div>
+
                 <h1>Test</h1>
+                {this.state.books.map((book, index) =>
+                    <div  key={index}>
+                        {book.bookName}
+                        <img src={book.image} alt=""/>
+                        </div>)}
             </div>
         )
     }
