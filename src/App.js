5612a15bb7ecdac00e2928eddf446e7a082ce37b
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-02-21 12:56:31 +0300

    add links in the home page + pull

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import Index from './book/Index';
-import Profile from './user/Profile';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div >
-     <Index></Index>
+      <nav>
+        <Router>
+          <div>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/book/index">Books</Link>{' '}
+            {/* <Link to="user/profile">Profile</Link>{' '} */}
+            {/* <Link to="/user/logout">Login</Link>{' '} */}
+            {/* <Link to="user/login">Logout</Link>{' '} */}
+            {/* <Link to="/user/register">Register</Link> */}
+          </div>
+          <div>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/book/index" component={Index}></Route>
+            {/* <Route path="user/profile" component={Profile}></Route> */}
+            {/* <Route path="user/logout" component={Login}></Route> */}
+            {/* <Route path="user/login" component={Logout}></Route> */}
+            {/* <Route path="user/register" component={Register}></Route> */}
+          </div>
+        </Router>
+      </nav>
 
-     {/* <Profile></Profile> */}
     </div>
   );
 }
