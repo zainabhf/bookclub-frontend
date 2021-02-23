d3a76da447ee95c89c922f56ffadcb44b3ce52dd
ghosoonfk <ghosooonf@gmail.com>
2021-02-23 14:34:16 +0300

    add edit review

--- src/review/EditReview.js
+++ src/review/EditReview.js
@@ -4,7 +4,13 @@ export default class EditReview extends Component {
     render() {
         return (
             <div>
-                
+                  <Container >
+                    <Form.Group >
+                        <Form.Label>Review</Form.Label>
+                        <Form.Control type="text" name="reviewContent"/>
+                        <Button variant="primary">Submit</Button>
+                    </Form.Group>
+                </Container>
             </div>
         )
     }
