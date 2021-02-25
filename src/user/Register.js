903c626f32648433f305689484d6558b7fca1933
ghosoonfk <ghosooonf@gmail.com>
2021-02-25 17:36:15 +0300

    fix the registration issue -moudi ghosoon atheer

--- src/user/Register.js
+++ src/user/Register.js
@@ -8,16 +8,26 @@ export default class Register extends Component {
         super(props)
 
         this.state = {
-            newUser: {}
+            newUser: {},
+            isMatch:null,
+            messege:null
+            
         }
     }
 
     registerHandler = () => {
+        if(this.checkPassword(this.state.newUser["password"],this.state.newUser["confirm"])){
         this.props.register(this.state.newUser);
+        }
+       else{
+           this.setState({
+               messege:"unmatch password"
+           })
+       }
     }
     changeHandler = (e) => {
 
-        let temp = { ... this.state.newUser }
+        let temp = {...this.state.newUser}
         temp[e.target.name] = e.target.value;
         temp["image"] = "https://thumbs.dreamstime.com/b/default-avatar-profile-image-vector-social-media-user-icon-potrait-182347582.jpg"
         temp["userRole"] = "ROLE_USER"
@@ -32,14 +42,45 @@ export default class Register extends Component {
 
         this.setState({
 
-            newUser: temp
+            newUser: temp,
+            isMatch:this.checkPassword(this.state.newUser["password"],this.state.newUser["confirm"])
 
         })
         console.log(temp);
+        console.log(this.state.isMatch);
+    }
+    checkPassword=(password,confirm)=>{
+        // const check={}
+        // check[e.target.name]=e.target.value
+    //   check["password"]=e.target.value
+    //   check["confirm"]=e.target.value
+    //   console.log(check["password"])
+    //   console.log(check["confirm"])
+// console.log(check["confirm"])
+// ("#confirm").val()
+      if(password== confirm){
+          return true
+      }else{
+          return false
+      }
+
     }
     render() {
+        
+        
+        const  successMessage=this.state.messege ?(
+           
+        
+            <Alert className="alert" variant="danger"> {this.state.messege}</Alert>
+           
+        ):null
+      
         return (
+            
             <div>
+                {successMessage}
+                <Alert variant="success">{this.state.messege}</Alert>
+
                 <Container>
                     <Form.Group>
                         <Form.Label>Name</Form.Label>
