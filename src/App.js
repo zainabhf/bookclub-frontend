e5c0e9f237f45d6051329126b4bbe23d38dd10e4
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-02-26 11:38:12 +0300

    Add review file - Moudi, Ghosoon, Atheer

--- src/App.js
+++ src/App.js
@@ -15,8 +15,67 @@ export default class App extends Component {
 
     this.state = {
       user: {},
+      isAuth: false,
+      userToken: "",
       redirect: ""
     }
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem("token");
+
+    if (token != null) {
+      let user = decode(token);
+
+      if (user) {
+        this.setState({
+          isAuth: true,
+          user: user,
+        });
+      } else if (!user) {
+        localStorage.removeItem("token");
+        this.setState({
+          isAuth: false,
+        });
+      }
+    }
+  }
+
+  login = (user) => {
+    axios
+      .post("bookclub/user/authenticate", user)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data.token);
+        console.log("Logged in successfully !")
+
+        if (response.data.token != null) {
+          localStorage.setItem("token", response.data.token);
+          let userToken = decode(response.data.token);
+
+          this.setState({
+            isAuth: true,
+            user: user,
+            userToken: userToken
+            // successMessage: "Successfully logged in!!!",
+            // message: null
+          });
+        } else {
+          this.setState({
+            isAuth: false,
+            userToken: null,
+            user: {},
+            // message: "Incorrect username or password",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          isAuth: false,
+          message: "Error Occured while login, please try again !",
+        });
+      });
   }
 
   register = (user) => {
@@ -27,7 +86,7 @@ export default class App extends Component {
         console.log("user information in axios of registeration")
         console.log(user)
         this.setState({
-          user: user,
+          // user: user,
           redirect: "./Login"
         })
       })
@@ -55,7 +114,7 @@ export default class App extends Component {
               <Route exact path="/" component={Home} />
               <Route path="/book/index" component={Index} />
               {/* <Route path="user/profile" component={Profile} /> */}
-              <Route path="/user/login" component={Login} />
+              <Route path="/user/login" component={() => <Login login={this.login} />} />
               {/* <Route path="user/logout" component={Logout} /> */}
               <Route path="/user/register" component={() => <Register register={this.register} />} />
             </div>
