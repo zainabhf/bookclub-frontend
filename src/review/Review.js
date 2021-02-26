930f3f028676060fa610b67fc554e91a74e2d61d
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-02-26 11:58:09 +0300

    Add component edit and add inside review class - Moudi, Ghosoon, Atheer

--- src/review/Review.js
+++ src/review/Review.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import EditReview from '../review/EditReview'
+import AddReview from '../review/AddReview'
 
 export default class Review extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
             <div>
+                {(this.props.isEditReview != true) ?
+                    <div>
+                        {this.props.review_book.map((review, index) =>
+                            <div>
+                                <p>{review.reviewContent}</p>
+                                <button
+                                    onClick={() => this.props.editViewReview(review)}
+                                >Edit</button>
+                                <button onClick={() => this.props.deleteReview(review)}>Delete</button>
+                            </div>
+                        )}
 
-            </div>
+                    </div>
+                    :
+                    <div>
+                        <EditReview
+                            review_book={this.props.review_book}
+                            book={this.props.book}
+                            submitEditReview={this.props.submitEditReview}
+                            reviewToBeEdited={this.props.reviewToBeEdited} editBookReview={this.props.editBookReview} />
+                    </div>
+                }
+
+                <hr />
+                <AddReview addReview={this.props.addReview} book={this.props.book} />
+
+            </div >
+
         )
     }
 }
