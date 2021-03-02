218ee57d5e3515e42d58271d4d930e396b3c2342
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-03-02 14:43:55 +0300

    Profile work

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Button, Container, Form, Col, Row } from 'react-bootstrap'
+import { Button, Container, Form } from 'react-bootstrap'
 import { Alert } from 'react-bootstrap';
 import '../book/Alert.css'
+import '../HomeStyle.css'
+import '../book/Detail?id='
 
 export default class Profile extends Component {
     constructor(props) {
@@ -14,10 +16,13 @@ export default class Profile extends Component {
             isEdit: false,
             isClick: false,
             errorMessage: null,
-            successMessage: null
+            successMessage: null,
+            reviewsOfUser: [],
+            books: this.props.books,
+            // clickOnBook: this.props.book,
+            redirect: this.props.redirect
         };
     }
-
 
     editViewProfile = () => {
         this.setState({
@@ -39,14 +44,13 @@ export default class Profile extends Component {
         this.setState({
             user: temp
         })
-        console.log(temp);
     }
 
     submitHandlerEditProfile = () => {
         this.setState({
             isEdit: false,
             isClick: false,
-              successMessage: 'Profile updated successfully !'
+            successMessage: 'Profile updated successfully !'
         })
         this.props.updateProfile(this.state.user)
     }
@@ -70,13 +74,9 @@ export default class Profile extends Component {
 
         temp[name] = e.target.value;
 
-        console.log(e.target.value)
-        console.log(e.target)
-
         this.setState({
             tempUserForEditingPassword: temp
         })
-        console.log(temp);
     }
 
     isMatchPassword = (password1, password2) => {
@@ -106,26 +106,62 @@ export default class Profile extends Component {
     render() {
         const successMessage = this.state.successMessage ? (
             <Alert className="alert" variant="success"> {this.state.successMessage}</Alert>
-          ) : null
-          const errorMessage = this.state.errorMessage ? (
+        ) : null
+        const errorMessage = this.state.errorMessage ? (
             <Alert className="alert" variant="danger">{this.state.errorMessage}</Alert>
-          ) : null
-      
-          return (
+        ) : null
+
+        return (
             <div >
-              {errorMessage}
-              {successMessage}
- 
+                {errorMessage}
+                {successMessage}
+
                 {(this.props.userToken != "") ?
+
                     <div>
+
+                        {console.log(this.state.user)}
+
                         {(this.state.isClick != true) ?
                             <div>
-                                <p>Name: {this.state.user.name}</p>
-                                <p>Role: {this.state.user.userRole}</p>
-                                <p>Email Address: {this.state.user.emailAddress}</p>
-                                <img src={this.state.user.image} alt="Profile Picture" />
-                                <button onClick={this.editViewProfile}>Edit Profile</button>
-                                <button onClick={this.editViewChangePassword}>Change Password</button>
+                                <img src="/background-profile.png" alt="back-ground profile" class="background-profile" />
+
+                                <div className="user-info-container">
+                                    <img src={this.state.user.image} alt="Profile Picture" className="profile-picture" />
+                                    <div id="button">
+                                        <button type="button" className="btn btn-secondary" onClick={this.editViewProfile}>Edit Profile</button>
+                                        <button type="button" className="btn btn-warning" onClick={this.editViewChangePassword}>Change Password</button>
+                                    </div>
+                                    <div className="user-info">
+                                        <p><b>Name:</b> {this.state.user.name}</p>
+                                        <p><b>Email Address:</b> {this.state.user.emailAddress}</p>
+                                        <p><b>Reviews:</b></p>
+                                        <div className="overflow">
+                                            {this.state.books.map((book) =>
+                                                <div>
+                                                    {book.review_book.map((review) =>
+                                                        <div >
+                                                            {(book.review_book.length == 0) ?
+                                                                <div></div>
+                                                                :
+                                                                <div>
+                                                                    {(review.user.id == this.state.user.id) ?
+                                                                        <div className="review-container">
+                                                                            <div className="review-book">{book.bookName}</div>
+                                                                            <div className="review-content">{review.reviewContent}</div>
+                                                                        </div>
+                                                                        :
+                                                                        <div></div>
+                                                                    }
+                                                                </div>
+                                                            }
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             :
                             <div>
@@ -137,7 +173,6 @@ export default class Profile extends Component {
                                                 <Form.Label>Name</Form.Label>
                                                 <Form.Control type="text" name="name" value={this.state.user.name} onChange={this.changeHandlerEditProfile}></Form.Control>
                                             </Form.Group>
-
                                             <Form.Group>
                                                 <Form.Label>Profile Image URL</Form.Label>
                                                 <Form.Control type="text" name="image" value={this.state.user.image} onChange={this.changeHandlerEditProfile}></Form.Control>
@@ -146,8 +181,6 @@ export default class Profile extends Component {
                                                 <Form.Label>Email Address</Form.Label>
                                                 <Form.Control type="email" name="emailAddress" value={this.state.user.emailAddress} onChange={this.changeHandlerEditProfile} disabled></Form.Control>
                                             </Form.Group>
-
-
                                             <Button variant="primary" block onClick={this.submitHandlerEditProfile}>Submit</Button>
                                         </Container >
                                     </div>
